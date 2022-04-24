0cf25e2995d91106e3fb3e62eb4aa9f629d67f8b
FunctionOverflu <ta.yu.gamedev@gmail.com>
2022-04-24 15:25:57 +0200

    Setup basic scrolling mech (complete), Added basic debug visual

--- Source/RollingRocker2D/ScrollActorLocationOnMoveForward.cpp
+++ Source/RollingRocker2D/ScrollActorLocationOnMoveForward.cpp
@@ -8,14 +8,29 @@ UScrollActorLocationOnMoveForward::UScrollActorLocationOnMoveForward()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
 }
 
+void UScrollActorLocationOnMoveForward::BeginPlay()
+{
+	m_CurrentAutoScrollSpeedMultiplier = m_InitialAutoScrollSpeedMultiplier;
+}
+
+void UScrollActorLocationOnMoveForward::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	if (!m_IsAutoScrolling)
+	{
+		return;
+	}
+
+	Scroll(FVector::DownVector * DeltaTime * m_CurrentAutoScrollSpeedMultiplier);
+}
+
 void UScrollActorLocationOnMoveForward::HandleOnMoveForward(FMoveForwardEventData moveForwardEventData)
 {
-	FVector movement = -FVector::UpVector * moveForwardEventData.MoveAmount * m_ScrollSpeedMultiplier;
+	FVector movement = -FVector::UpVector * moveForwardEventData.MoveAmount * m_MoveForwardScrollSpeedMultiplier;
 	Scroll(movement);
 }
 
--- Source/RollingRocker2D/ScrollActorLocationOnMoveForward.h
+++ Source/RollingRocker2D/ScrollActorLocationOnMoveForward.h
@@ -15,13 +15,29 @@ class ROLLINGROCKER2D_API UScrollActorLocationOnMoveForward : public UActorCompo
 
 protected:
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (ClampMin = "0", ClampMax = "10.0", UIMin = "0", UIMax = "10.0"))
-	float m_ScrollSpeedMultiplier = 1.0f;
+	float m_MoveForwardScrollSpeedMultiplier = 1.0f;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	bool m_IsAutoScrolling = true;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (ClampMin = "0", ClampMax = "100.0", UIMin = "0", UIMax = "100.0"))
+	float m_InitialAutoScrollSpeedMultiplier = 1.0f;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	float m_CurrentAutoScrollSpeedMultiplier = 1.0f;
 
 public:	
 	// Sets default values for this component's properties
 	UScrollActorLocationOnMoveForward();
 
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
 public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 	UFUNCTION(BlueprintCallable)
 	void HandleOnMoveForward(FMoveForwardEventData moveForwardEventData);
 
