c19a2de0b9fa0557d9f9f1e360172e6aac01c742
FunctionOverflu <ta.yu.gamedev@gmail.com>
2022-05-02 21:43:29 +0200

    Added life point pickup

---
+++ Source/RollingRocker2D/PickUpUser.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickUpUser.h"
+
+// Add default functionality here for any IPickUpUser functions that are not pure virtual.
---
+++ Source/RollingRocker2D/PickUpUser.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PickUpUser.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPickUpData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class AItemPickup* PickUp;
+};
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, Blueprintable)
+class UPickUpUser : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ROLLINGROCKER2D_API IPickUpUser
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void UsePickUp(FPickUpData pickUpData);
+};
